fac3793ff5fbc983ecd58431a322865e59f9f29d
Rikimk <rikimkd2@gmail.com>
2022-07-17 15:22:01 +0200

    yes

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-from re import L
+from cgitb import text
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':{'name': 'Sarthak'}}
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    # only get 10 published blogs
+    if published:
+        return {'data':f'{limit} published blogs from the list'}
+    else:
+        return {'data':f'{limit} blogs from the list'}
 
-@app.get('/about')
-def about():
-    return {'data':'about page'}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def about(id: int):
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+    return {'data':{'1','2'}}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data':f'Blog is created with title as {blog.title}'}
